dfc3bd8d683e068604e977f3ce4c719bb356d15c
Gabriela Fidalgo <fidalgo.gb@gmail.com>
2026-09-10 21:56:59 -0300

    Dia 6

--- Python/Exercício 21.py
+++ Python/Exercício 21.py
@@ -4,3 +4,10 @@ pygame.mixer.music.load('musica.mp3')
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy():
     pygame.time.Clock().tick(10)
+
+# import pygame
+# pygame.init()
+# pygame.mixer.music.load('musica.mp3')
+# pygame.mixer.music.play()
+# input()
+# pygame.event.wait()
