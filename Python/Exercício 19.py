e035ad020efa60e0523a54f9dd6365c3cff69221
Gabriela Fidalgo <fidalgo.gb@gmail.com>
2026-09-10 21:00:46 -0300

    Dia 6

---
+++ Python/Exercício 19.py
@@ -0,0 +1,6 @@
+from random import choice
+a = input('Digite o primeiro nome:')
+b = input('Digite o segundo nome:')
+c = input('Digite o terceiro nome:')
+d = input('Digite o quarto nome:')
+print('O nome sorteado foi {}.'.format(choice([a,b,c,d])))
---
+++ Python/Exercício 20.py
@@ -0,0 +1,8 @@
+from random import shuffle
+a = input('Digite o primeiro nome:')
+b = input('Digite o segundo nome:')
+c = input('Digite o terceiro nome:')
+d = input('Digite o quarto nome:')
+lista = [a,b,c,d]
+shuffle(lista)
+print('A ordem de apresentação será {}'.format(lista))
