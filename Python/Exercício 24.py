4eaf3166e2553d045b2155a4ac3bee482e1bb1d6
Gabriela Fidalgo <fidalgo.gb@gmail.com>
2026-09-13 22:00:45 -0300

    Dia 8

---
+++ Python/Exercício 24.py
@@ -0,0 +1,2 @@
+cidade = str(input('Digite o nome de uma cidade:')).strip().upper()
+print('Sua cidade começa com a palavra SANTO? {}'.format(cidade[:5] == 'SANTO'))
---
+++ Python/Exercício 25.py
@@ -0,0 +1,2 @@
+nome = str(input('Digite seu nome completo:')).strip().upper()
+print('Seu nome contém SILVA? {}'.format('SILVA' in nome))
